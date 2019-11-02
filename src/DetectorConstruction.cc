b726ee83a1d4a0a9f0f3755d6b42646392c61154
raymond1927 <raymondzhou227@gmail.com>
2019-11-02 16:02:53 +1100

    initial commit

---
+++ src/DetectorConstruction.cc
@@ -0,0 +1,170 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+/// \file electromagnetic/TestEm18/src/DetectorConstruction.cc
+/// \brief Implementation of the DetectorConstruction class
+//
+//
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include "DetectorConstruction.hh"
+#include "DetectorMessenger.hh"
+
+#include "G4Material.hh"
+#include "G4NistManager.hh"
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+
+#include "G4GeometryManager.hh"
+#include "G4PhysicalVolumeStore.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4SolidStore.hh"
+
+#include "G4UnitsTable.hh"
+#include "G4SystemOfUnits.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+DetectorConstruction::DetectorConstruction()
+:G4VUserDetectorConstruction(),fPBox(0), fLBox(0),
+ fDetectorMessenger(0), fSiMaterial(0)
+{
+  fBoxSize = 1*cm;
+  DefineMaterials();
+  SetMaterial("G4_AIR");
+  fDetectorMessenger = new DetectorMessenger(this);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+DetectorConstruction::~DetectorConstruction()
+{ delete fDetectorMessenger;}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* DetectorConstruction::Construct()
+{
+  return ConstructVolumes();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void DetectorConstruction::DefineMaterials()
+{
+
+    G4NistManager * man = G4NistManager::Instance();
+    G4Material * Si = man->FindOrBuildMaterial("G4_GALLIUM_ARSENIDE");
+
+    // Default materials in setup.
+    fSiMaterial = Si;
+
+
+  ////G4cout << *(G4Material::GetMaterialTable()) << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* DetectorConstruction::ConstructVolumes()
+{
+  // Cleanup old geometry
+  G4GeometryManager::GetInstance()->OpenGeometry();
+  G4PhysicalVolumeStore::GetInstance()->Clean();
+  G4LogicalVolumeStore::GetInstance()->Clean();
+  G4SolidStore::GetInstance()->Clean();
+
+  G4Box*
+  sBox = new G4Box("Container",                              //its name
+                   fBoxSize/2,fBoxSize/2,fBoxSize/2);        //its dimensions
+
+  fLBox = new G4LogicalVolume(sBox,                        //its shape
+                             fSiMaterial,                    //its material
+                             fSiMaterial->GetName());        //its name
+
+  fPBox = new G4PVPlacement(0,                          //no rotation
+                             G4ThreeVector(),           //at (0,0,0)
+                           fLBox,                       //its logical volume
+                           fSiMaterial->GetName(),        //its name
+                           0,                           //its mother  volume
+                           false,                       //no boolean operation
+                           0);                          //copy number
+                           
+  PrintParameters();
+  
+  //always return the root volume
+  //
+  return fPBox;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void DetectorConstruction::PrintParameters()
+{
+  G4cout << "\n The Box is " << G4BestUnit(fBoxSize,"Length")
+         << " of " << fSiMaterial->GetName()
+         << "\n "  << fSiMaterial << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void DetectorConstruction::SetMaterial(const G4String& name)
+{
+  // search the material by its name
+  G4Material* mat = G4Material::GetMaterial(name, false);
+
+  // create the material by its name
+  if(!mat) { mat = G4NistManager::Instance()->FindOrBuildMaterial(name); }
+
+  if(mat && mat != fSiMaterial) {
+    G4cout << "### New material " << mat->GetName() << G4endl;
+    fSiMaterial = mat;
+    UpdateGeometry();
+  }
+
+  if(!mat) {
+    G4cout << "\n--> warning from DetectorConstruction::SetMaterial : "
+           << name << " not found" << G4endl;  
+  } 
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void DetectorConstruction::SetSize(G4double value)
+{
+  fBoxSize = value;
+  UpdateGeometry();  
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include "G4RunManager.hh"
+
+void DetectorConstruction::UpdateGeometry()
+{
+  G4RunManager::GetRunManager()->GeometryHasBeenModified();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
